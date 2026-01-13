765969bad56d1f49a459dc6adb3f31ff01cb57ab
Hei-Lima <heitor.lipeixoto@gmail.com>
2026-01-13 00:23:14 -0300

    v1

--- esp32/senddata.ino
+++ esp32/senddata.ino
@@ -1,22 +1,55 @@
 #include <WiFi.h>
 #include "DHT.h"
 #include <HTTPClient.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 
+#define SCREEN_HEIGHT 64 
+
+#define SDA 6 // SDA pin
+#define SCL 5 // SCL pin (DUH)
+
+#define SCREEN_ADDRESS 0x3C 
+#define OLED_RESET     -1 
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 #define DHTPIN 4
 #define DHTTYPE DHT11
 
 DHT dht(DHTPIN, DHTTYPE);
-const char* SSID = "NETOCO";
-const char* PASSWORD = "a1b2c3d4";
-const char* ENVIRONMENT_POST = "http://192.168.0.185:8000/environment";
+const char* SSID = "xxxxxx";
+const char* PASSWORD = "xxxxx";
+const char* ENVIRONMENT_POST = "http://xxxxx:8000/environment";
+const char* API_TOKEN = "xxxxxx"; 
+
+int send_data(float h, float t, float hi);
 
 void setup() {
   Serial.begin(115200);
   dht.begin();
 
+  Wire.begin(SDA, SCL); 
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+
+  display.clearDisplay();
+  display.setTextColor(SSD1306_WHITE);
+  display.setTextSize(1);
+  display.display();
+
   WiFi.begin(SSID, PASSWORD);
   Serial.print("Connecting to WiFi");
   
+  display.setCursor(0, 0);
+  display.println("Connecting WiFi...");
+  display.display();
+
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -25,6 +58,16 @@ void setup() {
   Serial.println("\nConnected!");
   Serial.print("IP: ");
   Serial.println(WiFi.localIP());
+
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.setTextSize(1); 
+  display.println("WIFI CONNECTED!");
+  display.println("");    
+  display.println("IP Address:");
+  display.setTextSize(1); 
+  display.println(WiFi.localIP()); 
+  display.display();
 }
 
 void loop() {
@@ -50,14 +93,17 @@ void loop() {
 }
 
 int send_data(float h, float t, float hi) {
+  int httpResponseCode = 0;
+
   if (WiFi.status() == WL_CONNECTED) {
     HTTPClient http;
     http.begin(ENVIRONMENT_POST);
     http.addHeader("Content-Type", "application/json");
+    http.addHeader("Authorization", "Bearer " + String(API_TOKEN));
 
     String json = "{\"humidity\": " + String(h) + ", \"temperature\": " + String(t) + ", \"heatindex\": " + String(hi) + "}";
     
-    int httpResponseCode = http.POST(json);
+    httpResponseCode = http.POST(json);
     
     Serial.print("HTTP Response code: ");
     Serial.println(httpResponseCode);
@@ -65,5 +111,5 @@ int send_data(float h, float t, float hi) {
     http.end();
   }
 
-  return httpResponseCode
+  return httpResponseCode;
 }
